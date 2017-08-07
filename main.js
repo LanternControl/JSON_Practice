b73d480226a2c2820267e7c03bd20bc33019b112
LanternControl <brett4president@hotmail.com>
2017-08-07 16:03:59 -0400

    fixed url

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ fetch('https://randomuser.me/api/?results=1')
              
              <ul><h1>The Basics</h1>
                 <li>Name:${data.results[0].login.username}</li>
-                <li>Github URL:${data.results[0].characters}</li>
-                <li>Email:${data.results[0].email}</li>
+                <li>Registered:${data.results[0].registered}</li>
+                <li>Email:<a href="${data.results[0].email}">${data.results[0].email}</a></li>
                 <li>DOB:${data.results[0].dob}</li>
                 <li>Address:${data.results[0].location.street}</li>
             </ul>
@@ -25,7 +25,7 @@ fetch('https://randomuser.me/api/?results=1')
         <hr>
             <div class="second">
             <ul><h1>The Story</h1></ul>
- <img src =${data.results[0].picture.large}>
+            <img src =${data.results[0].picture.large}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae mauris vulputate, molestie lectus ut, faucibus ante. Aliquam scelerisque convallis justo eget varius. Donec lobortis placerat massa quis accumsan. Nam ac tellus velit. Nulla iaculis sodales ligula, vehicula sagittis arcu mattis ac. In finibus interdum elit laoreet vehicula. Donec posuere laoreet molestie. Donec laoreet elementum dictum. Integer vitae diam interdum, commodo risus vitae, eleifend augue.</p>
             
            
